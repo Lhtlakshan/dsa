d46f9630d213fe304ba168eb9f9fa95fe8801a52
Lhtlakshan <thilinalakshanhewage1999@gmail.com>
2025-02-13 22:56:54 +0530

    String

---
+++ src/main/java/string/StringClass.java
@@ -0,0 +1,27 @@
+package string;
+
+public class StringClass {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Ayllya"));;
+    }
+
+    public static boolean isPalindrome(String str){
+        if(str == null){
+            return true;
+        }
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length()/2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 -i);
+
+            if(start != end){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
