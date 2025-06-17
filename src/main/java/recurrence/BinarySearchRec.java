cc25b87447c781a9eea77e60d0a06e01a87086f8
Lhtlakshan <thilinalakshanhewage1999@gmail.com>
2025-06-17 22:22:42 +0530

    fact

--- src/main/java/recurrence/BinarySearchRec.java
+++ src/main/java/recurrence/BinarySearchRec.java
@@ -1,11 +1,14 @@
 package recurrence;
 
+import java.util.ArrayList;
+
 public class BinarySearchRec {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,5,7,8,9};
 
-        System.out.println(search(arr,9 ,0,6));
+       // System.out.println(search(arr,9 ,0,6));
+        print(5);
     }
 
     public static int search(int[] arr, int target , int start ,  int end){
@@ -24,5 +27,12 @@ public class BinarySearchRec {
          }
 
          return search(arr,target,start,mid-1);
+    }
+
+    public static void print(int n){
+        if(1<n){
+            print(n-1);
+        }
+        System.out.println(n);
     }
 }
---
+++ src/main/java/recurrence/Factorial.java
@@ -0,0 +1,15 @@
+package recurrence;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));;
+    }
+
+    public static int factorial(int n){
+        if(n<=2){
+            return n;
+        }
+        return n*factorial(n-1);
+    }
+}
