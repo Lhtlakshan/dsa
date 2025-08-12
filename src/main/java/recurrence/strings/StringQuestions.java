ac0e238e9e685c5494c3217761daf32d9ef19d8d
Lhtlakshan <thilinalakshanhewage1999@gmail.com>
2025-08-12 11:36:15 +0530

    some string questions

--- src/main/java/recurrence/strings/StringQuestions.java
+++ src/main/java/recurrence/strings/StringQuestions.java
@@ -1,4 +1,56 @@
 package recurrence.strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringQuestions {
+    public static void main(String[] args) {
+        subSeq("","abc");
+        List<List<Integer>> list = getAllSubSets(new int[]{1, 5, 2, 3});
+        for (List<Integer> i: list){
+            System.out.println(i);
+        }
+    }
+
+    //remove letter in from string
+    public static void removeLetter(String p,String up){
+        if(p.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            removeLetter(p,up.substring(1));
+        }else{
+            removeLetter(p+ch,up.substring(1));
+        }
+    }
+
+    public static void subSeq(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subSeq(p, up.substring(1));
+        subSeq(p + ch , up.substring(1));
+    }
+
+    public static List<List<Integer>> getAllSubSets(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for (int num: arr){
+            int size = outer.size();
+            for (int i=0; i<size; i++){
+                List<Integer> inner = new ArrayList<>(outer.get(i));
+                inner.add(num);
+                outer.add(inner);
+            }
+        }
+
+        return outer;
+    }
 }
--- src/main/java/sorting/Selection.java
+++ src/main/java/sorting/Selection.java
@@ -5,11 +5,15 @@ import java.util.Arrays;
 public class Selection {
 
     public static void main(String[] args) {
-        int []arr = {2,300,40,6,23,20};
+        int[] arr = {2,300,40,6,23,20};
+        int[] arr2 = {1,90,34,12,23};
+        selectionSortInOne(arr2);
+        System.out.println(Arrays.toString(arr2));
 
-        selectionSort(arr);
 
-        System.out.println(Arrays.toString(arr));
+//        selectionSort(arr);
+//
+//        System.out.println(Arrays.toString(arr));
     }
 
     static void selectionSort(int[] arr){
@@ -35,5 +39,23 @@ public class Selection {
         arr[first] = arr[second];
         arr[second] = temp;
 
+    }
+
+    //selection sort in one method
+    static void selectionSortInOne(int[] arr){
+        for(int i = 0; i< arr.length - 1; i++){
+            int min = i;
+
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+
+        }
     }
 }
